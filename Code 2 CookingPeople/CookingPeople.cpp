c45d61bb643f4f3bb603be3ad032164fb57c516b
wholmezy <wholmezy@gmail.com>
2014-05-29 12:52:03 +0200

    Accidentally uploaded the unfinished code.
    This is the right code!

--- Code 2 CookingPeople/CookingPeople.cpp
+++ Code 2 CookingPeople/CookingPeople.cpp
@@ -2,67 +2,73 @@
 #include <stdlib.h>
 #include <string.h>
 
+/************************************************************************************************************************************************
+** Recipe app that gets a recipe as input and outputs the same recipe with changed number of ingredients depending on the amount of people   ****
+** the input wants it to be for.																											 ****
+** Written by Wholmezy.																														 ****
+*************************************************************************************************************************************************/
+
 int main(){
 
+	//amount of people in the recipe and wanted for new recipie.
 	int ppl;
-	printf("For how many people is the recipie?\n");
+	printf("For how many people is the recipe?\n");
 	scanf("%d", &ppl);
+	printf("\n");
 
 	int newPpl;
-	printf("For how many people do you want the recipie to be for?\n");
-	scanf("%d", newPpl);
-	
-	printf("Press q to cancel.\nPress d to finish.\n");
+	printf("For how many people do you want the recipe to be for?\n");
+	scanf("%d", &newPpl);
+	printf("\n");
+
+	//Self-explanatory
+	printf("Press . to cancel.\nPress , to finish.\n\n");
 
 	bool doneFlag = true;
 
+	//incr is for the forloop later. Ingnum is the ingredient number.
 	int incr = 0;
 	int ingnum = 1;
 
-	int numBuff[20];
+	//numBuff is buffer for number of ingredients while prefNum is the opposite.
+	float numBuff[20];
 	char prefNum[10];
 
+	//ingredient stores the buffer for the name of the ingredient and what measure it is ingredPrnt is the opposite.
 	char ingredient[100];
 	char ingredPrnt[20][100];
 
 	while(doneFlag == true){
 
-		printf("lol\n");
-
+		//self-explanatory
 		printf("How many of the measure are there for ingredient number %d?\n", ingnum);
-
 		scanf("%s", &prefNum);
-		if(prefNum[0] == 'q'){ return 0; }
-		if(prefNum[0] == 'd'){ doneFlag = false; }
+
+		//checks if the program should finish asking for new recipe pieces or just quit.
+		if(prefNum[0] == '.'){ return 0; }
+		if(prefNum[0] == ','){ doneFlag = false; }
 		else{
-			numBuff[incr] = atoi(prefNum);
-			printf("%d\n", numBuff[incr]);
+			//make numBuff get the number of the specific ingredient then ask for the rest of the information.
+			numBuff[incr] = atof(prefNum);
 			printf("Give me the rest of the information; The measure and ingredient name!\n");
-
 			scanf(" %[^\n]", ingredient);
-			if(ingredient[0] == 'q'){ return 0; }
-			if(ingredient[0] == 'd'){ doneFlag = false; }
 
-			strcpy_s(ingredPrnt[incr], sizeof(ingredient), ingredient);
-			printf("%s\n", ingredPrnt[incr]);
-			printf("%s\n", ingredPrnt[0]);
+			if(ingredient[0] == '.'){ return 0; }
+			if(ingredient[0] == ','){ doneFlag = false; }
+
+			strcpy(ingredPrnt[incr], ingredient);
 
 			ingnum++;
 			incr++;
 		}
 	}
-
-
-	printf("Here is your newly improved recipie!\n\n");
+	//self-explanatory
+	printf("Here is your newly improved recipe!\n\n");
 	int i;
 	for(i = 0; i < incr; i++){
 		numBuff[i] = ((numBuff[i] / ppl) * newPpl);
-		printf("%d %s\n", numBuff[i], ingredPrnt[i]);
+		printf("%.2f %s\n", numBuff[i], ingredPrnt[i]);
 
 
 	}
-
-
-
-
 }
--- Code 2 CookingPeople/README.txt
+++ Code 2 CookingPeople/README.txt
@@ -1,1 +1,2 @@
 Pretty small but clean and fund to program program. I like it. But there is still time for improvement, the array which holds the number of ingredients should be flexible sized.
+Found out that my version of Code::Blocks is not up to date with the latest c version... I can not use strcpy_s or scanf_s, which are safer alternatives to strcpy and scanf.
